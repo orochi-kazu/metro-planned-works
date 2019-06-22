7c1b6eead1aa64c56a2a36af69f490ffa7a747ee
Orochi Kazuaki <1618373+orochi-kazu@users.noreply.github.com>
2019-06-22 22:47:03 +1000

    Updates

---
+++ precache-manifest.aefc1654c0e8871a8d0bbf16802cb09e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d1889102e9715ed645426108ba36b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da56c20a0f0d7971b571",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "da56c20a0f0d7971b571",
+    "url": "/static/js/main.2a0bd7a8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5901d73493d07605991e3413190369e6",
+    "url": "/static/media/logo.5901d734.svg"
+  }
+]);
