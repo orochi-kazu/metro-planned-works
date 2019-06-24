3afdad9dfb52c809c748e4bcdf93f22f39d84775
Orochi Kazuaki <1618373+orochi-kazu@users.noreply.github.com>
2019-06-24 23:46:45 +1000

    feat: 🎸 add base app layout

---
+++ planned-works-ui/src/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// import logo from './logo.svg'
+import './style.css'
+
+const Header = () => (
+  <header>
+    <nav>
+      {/* <img src={logo} alt='Alert icon' /> */}
+      Legible Works
+    </nav>
+  </header>
+)
+
+export { Header }
